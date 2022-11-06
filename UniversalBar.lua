a31ec3f2961db45185a68323016b821464d3f2b6
Pr3vention <12611124+Pr3vention@users.noreply.github.com>
2022-11-05 20:59:30 -0500

    Initial commit of addon

---
+++ UniversalBar.lua
@@ -0,0 +1,95 @@
+local addonName = select(1, ...)
+UniversalBar = select(2, ...)
+
+function UniversalBar:SetBarID(barID)
+	assert(barID >= 1 and barID <= 8, 'Invalid bar selected. You can only select bars 1 through 8')
+	UniversalBarSettings.BarID = barID
+end
+
+function UniversalBar:SaveBarConfig()
+	if not UniversalBarSettings.BarConfig then
+		UniversalBarSettings.BarConfig = {}
+	end
+	
+	local slot = 1
+	local startIndex = UniversalBarSettings.BarID * 12 + 1
+	local endIndex = startIndex + 11
+	for i = startIndex, endIndex do
+		local actionType, id, subType = GetActionInfo(i)
+		if actionType == 'summonmount' then
+			local spellID = select(2, C_MountJournal.GetMountInfoByID(id))
+			actionType = 'mount'
+			id = spellID
+		elseif actionType == 'item' then
+			if C_ToyBox.GetToyInfo(id) then
+				actionType = 'toy'
+			else
+				actionType = 'item'
+			end
+		end
+		if actionType then
+			UniversalBarSettings.BarConfig[slot] = {
+				type = actionType,
+				id = id
+			}
+		else 
+			UniversalBarSettings.BarConfig[slot] = nil
+		end
+		slot = slot + 1
+	end 
+end
+function UniversalBar:LoadBarConfig()
+	assert(UniversalBarSettings.BarID, 'Universal bar has not been set yet.')
+	assert(UniversalBarSettings.BarConfig, 'no bar config has been set')
+	
+	local needPlaceAction = true
+	for k,v in pairs(UniversalBarSettings.BarConfig) do
+		needPlaceAction = false
+		if v.type == 'spell' then
+			PickupSpell(v.id)
+			needPlaceAction = true
+		elseif v.type == 'mount' then
+			for i=1, C_MountJournal.GetNumMounts() do
+				local spellID = select(2, C_MountJournal.GetDisplayedMountInfo(i))
+				if spellID == v.id then
+					C_MountJournal.Pickup(i)
+					needPlaceAction = true
+					break;
+				end
+			end
+		elseif v.type == 'item' or v.type == 'toy' then
+			local item = Item:CreateFromItemID(v.id)
+			if not item:IsItemEmpty() then
+				local itemSlot = k
+				item:ContinueOnItemLoad(function()
+					if v.type == 'toy' then
+						C_ToyBox.PickupToyBoxItem(v.id)
+					else
+						PickupItem(v.id)
+					end
+					PlaceAction(UniversalBarSettings.BarID*12+itemSlot)
+					ClearCursor()
+				end)
+			end
+		elseif v.type == 'summonpet' then
+			C_PetJournal.PickupPet(v.id)
+			needPlaceAction = true
+		end
+		if needPlaceAction then
+			PlaceAction(UniversalBarSettings.BarID*12+k)
+			ClearCursor()
+		end
+	end
+end
+
+local eventFrame = CreateFrame("FRAME", "UniversalBarEventFrame", UIParent)
+eventFrame.events = {}
+eventFrame:RegisterEvent("ADDON_LOADED")
+eventFrame:SetScript("OnEvent", function(self, event, ...)
+	if event == 'ADDON_LOADED' and addonName == select(1,...) then
+		if not UniversalBarSettings then
+			UniversalBarSettings = {}
+		end
+		eventFrame:UnregisterEvent('ADDON_LOADED')
+	end
+end)
